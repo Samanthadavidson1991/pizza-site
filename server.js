b9d0aca6939d6d5e1540f854e56d451f2c30e44d
Samanthadavidson1991 <sammisandham@hotmail.com>
2025-08-30 00:39:02 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -101,16 +101,16 @@ app.post('/refund-order', async (req, res) => {
   }
 });
 
+
 // CORS middleware MUST be before express.json() and all routes
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(204);
-  }
-  next();
-});
+app.use(cors({
+  origin: [
+    'https://pizza-site-c8t6.onrender.com',
+    'http://thecrustatngb.co.uk',
+    'https://thecrustatngb.co.uk'
+  ],
+  credentials: true
+}));
 
 // Duplicate MongoDB connection setup removed
 app.use(express.static(__dirname));
